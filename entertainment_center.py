f0f3fce6377fe96aa3e55c9de66aff6cc5bbdd98
Jonathan Sullivan <jonathan.k.sullivan@gmail.com>
2016-01-04 17:24:38 -0600

    Add New Movies

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,8 +1,79 @@
 import media
 import fresh_tomatoes
 
+thirteen_hours = media.Movie("13 Hours",
+                             "https://upload.wikimedia.org/wikipedia/en/6/64/13_Hours_poster.jpg",
+                             "https://youtu.be/5MBjAN7jqsQ", "")
+jane_got_a_gun = media.Movie("Jane Got a Gun",
+                              "https://upload.wikimedia.org/wikipedia/en/7/79/Jane_got_a_Gun_Poster.jpg",
+                              "https://www.youtube.com/watch?v=fm7qxI3gmpw", "")
+kung_fu_panda_3 = media.Movie("Kung Fu Panda 3",
+                              "https://upload.wikimedia.org/wikipedia/en/e/e6/Kung_Fu_Panda_3_poster.jpg",
+                              "https://youtu.be/10r9ozshGVE", "")
+deadpool = media.Movie("Deadpool",
+                       "https://upload.wikimedia.org/wikipedia/en/4/46/Deadpool_poster.jpg",
+                       "https://youtu.be/ZIM1HydF9UA", "")
+hail_caesar = media.Movie("Hail, Caesar!",
+                          "https://upload.wikimedia.org/wikipedia/en/0/01/Hail%2C_Caesar%21_Teaser_poster.jpg",
+                          "https://www.youtube.com/watch?v=kMqeoW3XRa0", "")
+allegiant = media.Movie("The Divergent Series: Allegiant",
+                        "https://upload.wikimedia.org/wikipedia/en/f/f8/Allegiantfilmposter.jpg",
+                        "https://youtu.be/0G0C-vMHcQY", "")
+batman_v_superman_dawn_of_justice = media.Movie("Batman v Superman: Dawn of Justice",
+                                                "https://upload.wikimedia.org/wikipedia/en/2/20/Batman_v_Superman_poster.jpg",
+                                                "https://youtu.be/fis-9Zqu2Ro", "")
+demolition = media.Movie("Demolition",
+                         "http://static01.nyt.com/images/2015/07/29/arts/29DEMOLITION/29DEMOLITION-master675-v2.jpg",
+                         "https://www.youtube.com/watch?v=qDnV8LGDILE", "")
+the_jungle_book = media.Movie("The Jungle Book",
+                              "https://upload.wikimedia.org/wikipedia/en/a/a4/The_Jungle_Book_%282016%29.jpg",
+                              "https://youtu.be/5mkm22yO-bs", "")
+the_huntsman_winters_war = media.Movie("The Huntsman: Winter's War",
+                                       "https://upload.wikimedia.org/wikipedia/en/a/ab/The_Huntsman_%E2%80%93_Winter%27s_War_poster.jpg",
+                                       "https://www.youtube.com/watch?v=_W65ndip7MM", "")
+captain_america_civil_war = media.Movie("Captain America: Civil War",
+                                        "https://upload.wikimedia.org/wikipedia/en/5/53/Captain_America_Civil_War_poster.jpg",
+                                        "https://youtu.be/uVdV-lxRPFo", "")
+money_monster = media.Movie("Money Monster",
+                            "http://www.movienewz.com/img/films/money-monster-movie-poster-1.jpg",
+                            "https://www.youtube.com/watch?v=KzxmOG3Pm9I", "")
+the_nice_guys = media.Movie("The Nice Guys",
+                            "https://upload.wikimedia.org/wikipedia/en/e/e9/The_Nice_Guys_poster.png",
+                            "https://youtu.be/tyR-UqRom-c", "")
+alice_through_the_looking_glass = media.Movie("Alice Through the Looking Glass (film)",
+                                             "https://upload.wikimedia.org/wikipedia/en/b/b4/Alice_Through_the_Looking_Glass_%28film%29_poster.jpg",
+                                             "https://youtu.be/anvGUW-vsLE", "")
+xmen_apocalypse = media.Movie("X-Men: Apocalypse",
+                              "https://upload.wikimedia.org/wikipedia/en/0/04/X-Men_-_Apocalypse.jpg",
+                              "https://youtu.be/COvnHv42T-A", "")
+warcraft = media.Movie("Warcraft",
+                       "https://upload.wikimedia.org/wikipedia/en/5/56/Warcraft_Teaser_Poster.jpg",
+                       "https://youtu.be/RhFMIRuHAL4", "")
+finding_dory = media.Movie("Finding Dory",
+                           "https://upload.wikimedia.org/wikipedia/en/3/3e/Finding_Dory.jpg",
+                           "https://youtu.be/3JNLwlcPBPI","")
+independence_day_resurgence = media.Movie("Independence Day: Resurgence",
+                                          "https://upload.wikimedia.org/wikipedia/en/5/58/Independence-Day-2-poster.jpg",
+                                          "https://youtu.be/LbduDRH2m2M", "")
+the_bfg = media.Movie("The BFG ",
+                      "https://upload.wikimedia.org/wikipedia/en/5/5f/Titled_card_for_the_film.jpg",
+                      "https://youtu.be/VG5MtenlP-A", "")
+the_legend_of_tarzan = media.Movie("The Legend of Tarzan",
+                                   "https://upload.wikimedia.org/wikipedia/en/e/e5/The_Legend_of_Tarzan_poster.jpg",
+                                   "https://youtu.be/Aj7ty6sViiU", "")
+the_secret_life_of_pets = media.Movie("The Secret Life of Pets",
+                                      "https://upload.wikimedia.org/wikipedia/en/6/64/The_Secret_Life_of_Pets_poster.jpg",
+                                      "https://youtu.be/i-80SGWfEjM", "")
+ghostbusters_3 = media.Movie("Ghostbusters ",
+                             "https://upload.wikimedia.org/wikipedia/en/d/d0/Ghostbusters_2016_film_poster.jpg",
+                             "https://youtu.be/z5CJ3-vsjcw", "")
+star_trek_beyond = media.Movie("Star Trek Beyond",
+                               "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Star_Trek_Beyond_title_card.jpg/1024px-Star_Trek_Beyond_title_card.jpg",
+                               "https://youtu.be/XRVD32rnzOw", "")
+
 starwars7 = media.Movie("Star Wars: Episode VII",
                         "https://upload.wikimedia.org/wikipedia/en/a/a2/Star_Wars_The_Force_Awakens_Theatrical_Poster.jpg",
                         "https://youtu.be/sGbxmsDFVnE", "A continuation of the saga created by George Lucas and set thirty years after Star Wars: Episode VI - Return of the Jedi (1983).")
-movies = [starwars7]
+movies = [starwars7, thirteen_hours, jane_got_a_gun, kung_fu_panda_3, deadpool, hail_caesar, allegiant, batman_v_superman_dawn_of_justice, demolition, the_jungle_book, the_huntsman_winters_war, captain_america_civil_war, money_monster,
+          the_nice_guys, alice_through_the_looking_glass, xmen_apocalypse, warcraft, finding_dory, independence_day_resurgence, the_bfg, the_legend_of_tarzan, the_secret_life_of_pets, ghostbusters_3, star_trek_beyond]
 fresh_tomatoes.open_movies_page(movies)
